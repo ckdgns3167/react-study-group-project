4d97fa65a02f6812c26bbf91110ac1b69d7e4ba2
newckdgns <ckdgns3167@gmail.com>
2020-10-28 20:28:26 +0900

    Init my project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+
+const App = () => {
+  
+
+  return (
+    // <BrowserRouter>
+    //   <Switch>
+    //     <Route/>
+    //     <Route/>
+    //     <Route/>
+    //     <Route/>
+    //     <Route/>
+    //   </Switch>
+    // </BrowserRouter>
+    <div>asdasdasd</div>
+  )
+}
+
+export default App;
